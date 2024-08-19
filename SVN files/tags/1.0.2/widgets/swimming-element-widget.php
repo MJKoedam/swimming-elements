939f0a3bfee87f07801690e64b5ad772183115e3
MJKoedam <martjankoedam42@gmail.com>
2024-08-19 17:51:34 +0200

    Version 1.0.2
    Experimenting to get the icon to show up

---
+++ SVN files/tags/1.0.2/widgets/swimming-element-widget.php
@@ -0,0 +1,120 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class Elementor_Swimming_Element_Widget extends \Elementor\Widget_Base {
+
+    public function get_name() {
+        return 'swimming_element';
+    }
+
+    public function get_title() {
+        return __( 'Swimming Element', 'swimming-elements' );
+    }
+
+    public function get_icon() {
+        return 'eicon-animation';
+    }
+
+    public function get_categories() {
+        return [ 'basic' ];
+    }
+
+    protected function _register_controls() {
+
+        // Content Section
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => __( 'Content', 'swimming-elements' ),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        // Fish Content
+        $this->add_control(
+            'fish_content',
+            [
+                'label' => __( 'Fish Icon', 'swimming-elements' ),
+                'type' => \Elementor\Controls_Manager::WYSIWYG,
+                'default' => '<h1><b> <*͜𒅋 </b></h1>',
+                'label_block' => true,
+            ]
+        );
+
+        $this->end_controls_section(); // End Content Section
+
+        // Animation Settings Section
+        $this->start_controls_section(
+            'animation_settings_section',
+            [
+                'label' => __( 'Animation Settings', 'swimming-elements' ),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        // Mirrored Animation
+        $this->add_control(
+            'mirrored_animation',
+            [
+                'label' => __( 'Mirrored Animation', 'swimming-elements' ),
+                'type' => \Elementor\Controls_Manager::SWITCHER,
+                'label_on' => __( 'Yes', 'swimming-elements' ),
+                'label_off' => __( 'No', 'swimming-elements' ),
+                'default' => '',
+                'description' => __( 'Enable this to mirror the swimming animation.', 'swimming-elements' ),
+                'label_block' => true,
+            ]
+        );
+
+        // Animation Speed
+        $this->add_control(
+            'animation_speed',
+            [
+                'label' => __( 'Animation Speed (s)', 'swimming-elements' ),
+                'type' => \Elementor\Controls_Manager::NUMBER,
+                'default' => 0.8,
+                'step' => 0.1, // Step increment of 0.1
+                'description' => __( 'Set the animation speed in seconds.', 'swimming-elements' ),
+            ]
+        );
+
+        $this->end_controls_section(); // End Animation Settings Section
+
+    }
+
+    protected function render() {
+        $settings = $this->get_settings_for_display();
+
+        // Determine animation class based on mirrored animation setting
+        $animation_class = '';
+        if ( 'yes' === $settings['mirrored_animation'] ) {
+            $animation_class = 'mirrored';
+        }
+
+        // Inline styles for animation
+        $custom_styles = sprintf(
+            'style="--animation-speed: %ss;"',
+            esc_attr( $settings['animation_speed'] )
+        );
+
+        echo '<div class="swimming-container ' . esc_attr($animation_class) . '" ' . esc_attr($custom_styles) . '>';
+        echo '<div class="fish-icon">' . esc_attr($settings['fish_content']) . '</div>';
+        echo '</div>';
+    }
+
+    protected function _content_template() {
+        ?>
+        <#
+        var animationClass = settings.mirrored_animation ? 'mirrored' : '';
+        var customStyles = `style="--animation-speed: ${settings.animation_speed}s;"`;
+        #>
+        <div class="swimming-container {{ animationClass }}" {{{ customStyles }}}>
+            <div class="fish-icon">
+                {{{ settings.fish_content }}}
+            </div>
+        </div>
+        <?php
+    }
+
+}
+?>
--- SVN files/trunk/readme.txt
+++ SVN files/trunk/readme.txt
@@ -1,38 +1,33 @@
-# Swimming Elements
-
-**Contributors:** mjk01  
-**Tags:** elementor, widget, animation, swimming, elements  
-**Requires at least:** 5.0  
-**Tested up to:** 6.5.5  
-**Requires PHP:** 7.0  
-**Stable tag:** 1.0.1
-**License:** GPLv2 or later  
-**License URI:** https://www.gnu.org/licenses/gpl-2.0.html
+=== Swimming Elements ===
+Contributors: mjk01
+Tags: elementor, widget, animation, swimming, elements
+Requires at least: 5.0
+Tested up to: 6.6.1
+Requires PHP: 7.0
+Stable tag: 1.0.2
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 Add swimming animation elements to your Elementor-powered website easily with this plugin.
 
-## Description
-
+== Description ==
 Swimming Elements is a plugin that allows you to add animated swimming elements to your Elementor-designed pages.
 You can customize the animation speed and direction of the swimming-movement.
 Note: At this moment, the plugin only gives a static movement for the element and does not actually make the element swim throughout the page.
 
-## Installation
-
+== Installation ==
 1. Upload the `swimming-elements` folder to the `/wp-content/plugins/` directory.
-2. Activate the plugin through the 'Plugins' menu in WordPress.
-3. Drag the "Swimming Element" widget into your Elementor layout.
+2. Activate the plugin through the \'Plugins\' menu in WordPress.
+3. Drag the \"Swimming Element\" widget into your Elementor layout.
 4. Customize the widget settings to configure the swimming animation as desired.
 5. Save your changes and preview your Elementor-designed page with the swimming animation.
 
-## Changelog
+== Changelog ==
+### 1.0.2
+- Add icon to the plugin.
 
 ### 1.0.1
 - Add escape functions for all variables.
 
 ### 1.0.0
 - Initial release of Swimming Elements plugin.
-
-## License
-
-Swimming Elements is licensed under the GPL-2.0+ License. See [GPL-2.0 License](https://www.gnu.org/licenses/gpl-2.0.html) for more information.
